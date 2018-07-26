f9c611e78f29f3a3a56523f2d05887375e24b94a
tiennx001 <tiennx.it@gmail.com>
2018-07-26 21:43:27 +0700

    [[WEB] Sửa ID facebook app

--- public_html/wp-content/themes/yootheme/templates/extras/_facebook_init.php
+++ public_html/wp-content/themes/yootheme/templates/extras/_facebook_init.php
@@ -3,6 +3,6 @@
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.0&appId=2030440677170156&autoLogAppEvents=1';
+    js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.0&appId=212953692747786&autoLogAppEvents=1';
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));</script>
